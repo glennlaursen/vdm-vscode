4b5c237a79d44e59363f91246e81a328ffa000f1
Laursen <201703930@post.au.dk>
2023-03-27 13:38:49 +0200

    Remove old script. Remove old jar json structure

--- resources/jars/new-jar.js
+++ resources/jars/new-jar.js
@@ -6,29 +6,36 @@ const fs = require("fs");
 var args = process.argv.slice(2);
 
 if (args.length < 1) {
-    throw new Error("repository argument required");
+    throw new Error("Arguments required");
 }
 
-let repo = args.find((a) => a.includes("repo=")).slice("repo=".length);
-let groupId = args.find((a) => a.includes("groupId=")).slice("groupId=".length);
-let artifactId = args.find((a) => a.includes("artifactId=")).slice("artifactId=".length);
+let configId = args.find((a) => a.includes("configId=")).slice("configId=".length);
 let version = args.find((a) => a.includes("version=")).slice("version=".length);
 
-let jarInfo = data.jars.find((j) => j.repository === repo);
+
+let jarInfo = data.jars[configId];
 
 if (!jarInfo) {
-    throw new Error("Repository is not allowed");
+    throw new Error("No config found.");
 }
 
-if (jarInfo.classifier && jarInfo.classifier.includes('-')) {
-    downloadWithClassifier(jarInfo.repository, groupId, artifactId, version, jarInfo.classifier, jarInfo.destDir);
+if (jarInfo.classifier && jarInfo.classifier.length > 0) {
+    jarInfo.jarDestinations.forEach((dest) => {
+        dest.artifactIds.split(',').forEach((artifactId) => {
+            downloadWithClassifier(jarInfo.repository, jarInfo.groupId, artifactId, version, jarInfo.classifier, dest.destDir);
+        });
+    });
 } else {
-    download(jarInfo.repository, groupId, artifactId, version, jarInfo.destDir);
+    jarInfo.jarDestinations.forEach((dest) => {
+        dest.artifactIds.split(',').forEach((artifactId) => {
+            download(jarInfo.repository, jarInfo.groupId, artifactId, version, dest.destDir);
+        });
+    });
 }
 
 
 function download(repository, groupId, artifactId, version, destDir) {
-    const output = execSync(`mvn dependency:get \
+    const output = execSync(`mvn --batch-mode dependency:get \
     -Dartifact=${groupId}:${artifactId}:${version} \
     -DremoteRepositories=github::default::https://maven.pkg.github.com/${repository} \
     -Ddest=${destDir}/${artifactId}-${version}.jar`, {encoding: 'utf-8'});
